91c337c29f1f5d4ceb10840a98cbf9834aba9316
Emil Williams <emil@chud.cyou>
2026-05-01 18:46:01 +0000

    Profoundly Special Check

--- main.c
+++ main.c
@@ -9,9 +9,10 @@ clang -Wall -Wextra -Wpedantic -std=c23 -o $*.out $@
 #include "terry.h"
 #include <stdio.h>
 signed main(void) {
+  printf("%ld\n", __STDC_VERSION__);
   u128 a; (void) a;
   f32 b; (void) b;
   f80 c; (void) c;
-  d32 d; (void) d;
+  /* d32 d; (void) d; */
   return 0;
 }
--- terry.h
+++ terry.h
@@ -1,7 +1,7 @@
 #ifndef TERRY_H
 #define TERRY_H
 
-#if __STDC_VERSION__ < 202311L
+#if !(__STDC_VERSION__ >= 202311L)
 #error >=C23 Required
 #endif
 
